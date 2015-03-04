90127cb1172c2196d8012d04ee2976a89490bdec
mdt <mdt@wegewerk.com>
2015-03-04 14:39:40 +0000

    Bumped Version to 1.0

--- Firefox/lib/settings.js
+++ Firefox/lib/settings.js
@@ -2,4 +2,4 @@ exports.include = ["http://*","https://*"];
 exports.contentScriptWhen = "end";
 exports.contentScriptFile = ["BabelExt.js","jquery.js","jquery_highlight.js","jquery.tooltipster.min.js","fg_page.js","moment.js"];
 exports.contentStyleFile = ["style.css","tooltipster.css"];
-exports.icons = {"16":"lobbyradar-16.png","16m":"lobbyradar-16-mono.png","32":"lobbyradar-32.png","38":"lobbyradar-38.png","48":"lobbyradar-48.png","64":"lobbyradar-64.png","128":"lobbyradar-128.png"};
+exports.icons = {"16":"lobbyradar-16.png","32":"lobbyradar-32.png","38":"lobbyradar-38.png","48":"lobbyradar-48.png","64":"lobbyradar-64.png","128":"lobbyradar-128.png"};
